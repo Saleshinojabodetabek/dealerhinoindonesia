1d75dd6bb8c9c8f8e9ac2146fdab3b1086d68b46
amanrent <amanrentc@gmail.com>
2025-08-22 14:49:54 +0700

    Commit

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+// footer.php
+?>
+    <!-- Footer -->
+    <footer>
+      <div class="footer-container">
+        <div class="footer-section footer-logo">
+          <img src="images/logo3.png" alt="Hino Logo" loading="lazy" />
+          <p>Nathan Hino siap membantu Anda mendapatkan truk Hino baru dengan harga kompetitif untuk seluruh Indonesia...</p>
+        </div>
+        <div class="footer-section footer-address">
+          <h3>Alamat</h3>
+          <p><i data-feather="map-pin"></i> Golf Lake Ruko Venice, Jl. Lkr. Luar Barat No.78...</p>
+          <div class="social-icons">
+            <a href="https://www.instagram.com/saleshinojabodetabek"><i data-feather="instagram"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=61573843992250"><i data-feather="facebook"></i></a>
+            <a href="https://api.whatsapp.com/send/?phone=%2B6285975287684"><i data-feather="phone"></i></a>
+            <a href="https://www.tiktok.com/@saleshinoindonesia"><i data-feather="youtube"></i></a>
+          </div>
+        </div>
+        <div class="footer-section footer-contact">
+          <h3>Kontak</h3>
+          <p><i data-feather="phone"></i> Telephone : 0882-1392-5184</p>
+          <p><i data-feather="message-circle"></i> Whatsapp : 0859-7528-7684</p>
+          <p><i data-feather="mail"></i> Email : saleshinojabodetabek@gmail.com</p>
+        </div>
+        <div class="footer-section footer-barcode">
+          <h3>Scan Barcode</h3>
+          <img src="images/barcode.jpeg" alt="Barcode" loading="lazy" class="barcode-img"/>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p>© <?php echo date("Y"); ?> Dealer Hino Indonesia. All rights reserved.</p>
+      </div>
+    </footer>
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script>feather.replace();</script>
+  </body>
+</html>

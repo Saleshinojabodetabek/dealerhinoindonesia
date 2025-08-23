7995fe419fc3b66047cd6eb14d0fdda89bbfec28
amanrent <amanrentc@gmail.com>
2025-08-23 22:24:22 +0700

    Commit

--- admin/tambah_produk.php
+++ admin/tambah_produk.php
@@ -3,9 +3,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
+if (!isset($_SESSION['admin'])) {
+    header("Location: login.php");
+    exit();
+}
+include 'koneksi.php';
 
-
-<?php
 session_start();
 if (!isset($_SESSION['admin'])) {
     header("Location: login.php");
